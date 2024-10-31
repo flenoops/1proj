79c70d3171b80f05f754ce309722c0bcedab6596
flenoops <flenchikv@gmail.com>
2024-10-31 18:09:44 +0300

    s1

---
+++ проэкт/сайты/4.затемненная кнопка/script.js
@@ -0,0 +1,12 @@
+function hoverOnLeft(){
+	document.getElementById("div_right").classList.add("active");
+}
+function hoverOffLeft(){
+	document.getElementById("div_right").classList.remove("active");
+}
+function hoverOnRight(){
+	document.getElementById("div_left").classList.add("active");
+}
+function hoverOffRight(){
+	document.getElementById("div_left").classList.remove("active");
+}
